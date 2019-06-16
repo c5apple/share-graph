b58753eddd79fe8c249b82b4b4f28458a3715506
c5apple <c5apple_party@yahoo.co.jp>
2019-06-16 19:24:18 +0900

    ヘッダー

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { HotTableModule } from '@handsontable/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
