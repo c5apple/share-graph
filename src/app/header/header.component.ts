3a2d20863a71e4552a59ac476e392a870ad6cac8
c5apple <c5apple_party@yahoo.co.jp>
2019-06-17 05:59:17 +0900

    サイドナビ

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  authenticated = false;
+
   constructor() { }
 
   ngOnInit() {
+    document.addEventListener('DOMContentLoaded', function () {
+      var elems = document.querySelectorAll('.sidenav');
+      var instances = window['M'].Sidenav.init(elems, {});
+    });
   }
 
 }
