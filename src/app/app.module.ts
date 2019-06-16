9848695c60c86e84233a73b3875f3675f9a324c2
c5apple <c5apple_party@yahoo.co.jp>
2019-06-16 22:51:56 +0900

    フッター

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { GraphComponent } from './graph/graph.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    GraphComponent
+    GraphComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
