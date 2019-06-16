acb39016ccf470c25df910436348fb0b6c567c0c
c5apple <c5apple_party@yahoo.co.jp>
2019-06-16 20:21:25 +0900

    データ連動

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,16 +15,7 @@ export class AppComponent implements AfterViewInit {
   id = 'my-custom-id';
 
   hotSettings: Handsontable.GridSettings = {
-    data: [
-      ["", "広", "巨", "横", "阪", "ヤ", "中"],
-      ['2012', 61, 86, 46, 55, 68, 75],
-      ['2013', 69, 84, 64, 73, 57, 64],
-      ['2014', 74, 82, 67, 75, 60, 67],
-      ['2015', 69, 75, 62, 70, 76, 62],
-      ['2016', 89, 71, 69, 64, 64, 58],
-      ['2017', 88, 72, 73, 78, 45, 59],
-      ['2018', 82, 67, 67, 62, 75, 63],
-    ],
+    data: [],
     colHeaders: true,
     rowHeaders: true,
     stretchH: 'all',
@@ -44,45 +35,81 @@ export class AppComponent implements AfterViewInit {
       backgroundColor: 'rgb(254, 0, 2)',
       borderColor: 'rgb(254, 0, 2)',
       fill: false,
-      data: [61, 69, 74, 69, 89, 88, 82],
+      data: [],
     }, {
       label: "巨",
       backgroundColor: 'rgb(255, 101, 1)',
       borderColor: 'rgb(255, 101, 1)',
       fill: false,
-      data: [86, 84, 82, 75, 71, 72, 67],
+      data: [],
     }, {
       label: "横",
       backgroundColor: 'rgb(4, 78, 199)',
       borderColor: 'rgb(4, 78, 199)',
       fill: false,
-      data: [46, 64, 67, 62, 69, 73, 67],
+      data: [],
     }, {
       label: "阪",
       backgroundColor: 'rgb(255, 255, 0)',
       borderColor: 'rgb(1, 1, 1)',
       fill: false,
-      data: [55, 73, 75, 70, 64, 78, 62],
+      data: [],
     }, {
       label: "ヤ",
       backgroundColor: 'rgb(10, 12, 131)',
       borderColor: 'rgb(10, 12, 131)',
       fill: false,
-      data: [68, 57, 60, 76, 64, 45, 75],
+      data: [],
     }, {
       label: "中",
       backgroundColor: 'rgb(0, 0, 172)',
       borderColor: 'rgb(0, 0, 172)',
       fill: false,
-      data: [75, 64, 67, 62, 58, 59, 63],
+      data: [],
     }]
   };
   options: ChartOptions = {};
   chart: Chart;
 
   ngAfterViewInit(): void {
+    const data = [
+      ["", "広", "巨", "横", "阪", "ヤ", "中"],
+      ['2012', 61, 86, 46, 55, 68, 75],
+      ['2013', 69, 84, 64, 73, 57, 64],
+      ['2014', 74, 82, 67, 75, 60, 67],
+      ['2015', 69, 75, 62, 70, 76, 62],
+      ['2016', 89, 71, 69, 64, 64, 58],
+      ['2017', 88, 72, 73, 78, 45, 59],
+      ['2018', 82, 67, 67, 62, 75, 63],
+    ];
+
+    // テーブルデータ設定
+    this.swapHotData(data);
+    this.updateHotSettings();
+
+    // チャートデータ設定
     const canvas = this.myCanvas.nativeElement;
     this.drawChart(canvas);
+    this.swapChartData();
+  }
+
+  swapHotData(data: any[]): void {
+    const hot = this.hotRegisterer.getInstance(this.id);
+    if (hot) {
+      hot.loadData(data);
+    }
+  }
+
+  updateHotSettings(): void {
+    const hot = this.hotRegisterer.getInstance(this.id);
+    if (hot) {
+      hot.updateSettings({
+        afterChange: (changes, source) => {
+          // チャートデータリフレッシュ
+          this.swapChartData();
+        }
+      } as Handsontable.GridSettings, false);
+    }
   }
 
   drawChart(ctx: string | CanvasRenderingContext2D | HTMLCanvasElement | ArrayLike<CanvasRenderingContext2D | HTMLCanvasElement>) {
@@ -91,5 +118,24 @@ export class AppComponent implements AfterViewInit {
       data: this.data,
       options: this.options
     });
+  }
+
+  swapChartData() {
+    const hot = this.hotRegisterer.getInstance(this.id);
+    if (hot) {
+      const transpose = a => a[0].map((_, c) => a.map(r => r[c]));
+      let transData: any[] = transpose(hot.getData());
+
+      const labels = transData.shift();
+      // console.log('labels', labels);
+
+      // データリフレッシュ
+      this.chart.data.datasets.forEach((dataset) => {
+        const row = transData.shift();
+        row.shift();
+        dataset.data = row;
+      });
+      this.chart.update();
+    }
   }
 }
