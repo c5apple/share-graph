168edabf416e7dc8d771597265d14461bc90c47d
c5apple <c5apple_party@yahoo.co.jp>
2019-06-16 14:25:34 +0900

    データセット

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,24 +16,18 @@ export class AppComponent implements AfterViewInit {
 
   hotSettings: Handsontable.GridSettings = {
     data: [
-      { id: 1, 'name.first': 'Ted', 'name.last': 'Right' },
-      { id: 2, 'name.first': 'Frank', 'name.last': 'Honest' },
-      { id: 3, 'name.first': 'Joan', 'name.last': 'Well' },
-      { id: 4, 'name.first': 'Gail', 'name.last': 'Polite' },
-      { id: 5, 'name.first': 'Michael', 'name.last': 'Fair' },
-      { id: 6, 'name.first': 'Mia', 'name.last': 'Fair' },
-      { id: 7, 'name.first': 'Cora', 'name.last': 'Fair' },
-      { id: 8, 'name.first': 'Jack', 'name.last': 'Right' },
+      ["", "広", "巨", "横", "阪", "ヤ", "中"],
+      ['2012', 61, 86, 46, 55, 68, 75],
+      ['2013', 69, 84, 64, 73, 57, 64],
+      ['2014', 74, 82, 67, 75, 60, 67],
+      ['2015', 69, 75, 62, 70, 76, 62],
+      ['2016', 89, 71, 69, 64, 64, 58],
+      ['2017', 88, 72, 73, 78, 45, 59],
+      ['2018', 82, 67, 67, 62, 75, 63],
     ],
-    height: 320,
     colHeaders: true,
     rowHeaders: true,
     stretchH: 'all',
-    columns: [
-      { data: 'id', title: 'ID', type: 'numeric', readOnly: true },
-      { data: 'name.first', title: 'First name' },
-      { data: 'name.last', title: 'Last name' },
-    ],
     contextMenu: true,
     fillHandle: false,
     manualColumnResize: true,
@@ -44,12 +38,43 @@ export class AppComponent implements AfterViewInit {
 
   type: ChartType | string = 'line';
   data: ChartData = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    labels: ["2012", "2013", "2014", "2015", "2016", "2017", "2018"],
     datasets: [{
-      label: "My First dataset",
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgb(255, 99, 132)',
-      data: [0, 10, 5, 2, 20, 30, 45],
+      label: "広",
+      backgroundColor: 'rgb(254, 0, 2)',
+      borderColor: 'rgb(254, 0, 2)',
+      fill: false,
+      data: [61, 69, 74, 69, 89, 88, 82],
+    }, {
+      label: "巨",
+      backgroundColor: 'rgb(255, 101, 1)',
+      borderColor: 'rgb(255, 101, 1)',
+      fill: false,
+      data: [86, 84, 82, 75, 71, 72, 67],
+    }, {
+      label: "横",
+      backgroundColor: 'rgb(4, 78, 199)',
+      borderColor: 'rgb(4, 78, 199)',
+      fill: false,
+      data: [46, 64, 67, 62, 69, 73, 67],
+    }, {
+      label: "阪",
+      backgroundColor: 'rgb(255, 255, 0)',
+      borderColor: 'rgb(1, 1, 1)',
+      fill: false,
+      data: [55, 73, 75, 70, 64, 78, 62],
+    }, {
+      label: "ヤ",
+      backgroundColor: 'rgb(10, 12, 131)',
+      borderColor: 'rgb(10, 12, 131)',
+      fill: false,
+      data: [68, 57, 60, 76, 64, 45, 75],
+    }, {
+      label: "中",
+      backgroundColor: 'rgb(0, 0, 172)',
+      borderColor: 'rgb(0, 0, 172)',
+      fill: false,
+      data: [75, 64, 67, 62, 58, 59, 63],
     }]
   };
   options: ChartOptions = {};
